113c8cd75f2ec777381065fb45ec60161edd052a
UNIM-AJL <161278737+UNIM-AJL@users.noreply.github.com>
2024-05-27 16:52:58 +1000

    product page responsiveness

--- product-page/script.js
+++ product-page/script.js
@@ -32,3 +32,27 @@ closeBtn.onclick = function(){
 menuToggle.onclick = function(){
     header.classList.toggle('open');
 }
+
+
+
+
+const plus = document.querySelector(".plus"),
+    minus = document.querySelector(".minus"),
+    num = document.querySelector(".num");
+
+
+    let a = 1;
+
+
+    plus.addEventListener("click", ()=>{
+        a++;
+        num.innerText = a;
+        console.log(a)
+    });
+
+    minus.addEventListener("click", ()=>{
+        if(a>1){
+            a--;
+            num.innerText = a;
+        }
+    });
