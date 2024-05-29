6064e21d261b679afda3c969960da230b59b4f4d
UNIM-AJL <161278737+UNIM-AJL@users.noreply.github.com>
2024-05-30 09:46:27 +1000

    checkout page two 2nd commit

--- checkout-page-two/script.js
+++ checkout-page-two/script.js
@@ -19,3 +19,21 @@ minimiseBag.onclick = function(){
     expandBag.classList.remove('active');
     minimiseBag.classList.remove('active');
 }
+
+
+
+
+let creditCard = document.querySelector('.credit-card')
+
+let creditCardForm = document.querySelector('.credit-card-details-form')
+
+let alternativePaymentMethod = document.querySelector('.altnerative-payment-method')
+
+
+creditCard.onclick = function(){
+    creditCardForm.classList.add('active');
+}
+
+alternativePaymentMethod.onclick = function(){
+    creditCardForm.classList.remove('active')
+}
